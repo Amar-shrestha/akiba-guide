1a8c72ce2de59acc5642066090ad05fdd620a103
Amar-shrestha <shresthaamar12345@gmail.com>
2021-10-22 10:32:01 +0545

    initial files

---
+++ assets/js/common.js
@@ -0,0 +1,31 @@
+jQuery(document).ready(function () {
+	function setHeight() {
+		windowHeight = jQuery(window).innerHeight();
+        // leaveHeight = jQuery('#main-header').innerHeight();
+        // bannerHeight = windowHeight-leaveHeight; 
+		bannerHeight = windowHeight; 
+		jQuery('#main-banner .table-wrapper').css('height', bannerHeight);  
+	} 
+	setHeight();
+
+	jQuery(window).resize(function () {
+		setHeight();
+	});
+});
+
+  
+jQuery(document).ready(function($) {
+	$(".scroll").click(function(event){		
+	event.preventDefault();
+	$('html,body').animate({scrollTop:$(this.hash).offset().top},1000);
+	});
+});
+
+
+
+jQuery(document).ready(function () {
+	jQuery('nav').meanmenu();
+});
+
+
+
